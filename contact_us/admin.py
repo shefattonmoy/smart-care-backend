2558ac8d122ff7b3ffe794a0e1ac5632022bed46
shefattonmoy <shefathossain7@gmail.com>
2024-07-02 17:54:22 +0600

    API Implementation Done

---
+++ contact_us/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import ContactUs
+
+# Register your models here.
+
+class ContactModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'problem']
+
+admin.site.register(ContactUs, ContactModelAdmin)
