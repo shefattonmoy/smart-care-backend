fcc6974395100f9176580d42dad3dd1d5da5a5d7
shefattonmoy <shefathossain7@gmail.com>
2024-07-05 22:45:51 +0600

    done

--- patient/urls.py
+++ patient/urls.py
@@ -8,4 +8,8 @@ router.register('list', views.PatientViewset)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('register', views.UserRegistrationApiView.as_view(), name = 'register'),
+    path('login', views.UserLoginApiView.as_view(), name = 'login'),
+    path('logout', views.UserLogoutApiView.as_view(), name = 'logout'),
+    path('active/<uid64>/<token>/', views.activate, name = 'activate'),
 ]
--- smart_care/settings.py
+++ smart_care/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-hd(uk#9k0s)&@0$#$1m&f1=f^#2+zb0)82+l4uw-is*ne5rm6o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'appointment',
     'contact_us',
     'doctor',
@@ -86,6 +90,10 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -128,3 +136,10 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = env("EMAIL")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
